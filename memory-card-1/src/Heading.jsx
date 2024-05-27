b95945d908aa62c8fed484d4821127d760759fce
Derek Lim <dereklim3006@gmail.com>
2024-05-27 07:39:03 -0400

    Write heading.

---
+++ memory-card-1/src/Heading.jsx
@@ -0,0 +1,13 @@
+export default function Heading() {
+  return (
+    <>
+      <div>AOT Memory Game</div>
+      <div>Get points by clicking on an image 
+        but don't click on any more than once.</div>
+      <div>
+        <div>Score: 0</div>
+        <div>Best score: 0</div>
+      </div>
+    </>
+  )
+}
