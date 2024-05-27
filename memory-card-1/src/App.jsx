f5270577e904ab19c193ac407a57937d5e235eda
Derek Lim <dereklim3006@gmail.com>
2024-05-27 09:10:10 -0400

    Create cards.

--- memory-card-1/src/App.jsx
+++ memory-card-1/src/App.jsx
@@ -1,11 +1,13 @@
 // import { useState } from 'react'
 import Heading from './Heading.jsx'
+import Cards from './Cards.jsx'
 import './App.css'
 
 function App() {
   return (
     <>
       <Heading />
+      <Cards />
     </>
   )
 }
---
+++ memory-card-1/src/Cards.jsx
@@ -0,0 +1,20 @@
+import Card from './Card.jsx'
+
+export default function Cards() {
+  return (
+    <div className='container'>
+      <Card name='Eren Jaegar' />
+      <Card name='Mikasa Ackerman' />
+      <Card name='Armin Arlet' />
+      <Card name='Reiner Braun' />
+      <Card name='Jean Kirstein' />
+      <Card name='Connie Springer' />
+      <Card name='Sasha Blouse' />
+      <Card name='Historia Reiss' />
+      <Card name='Floch Forster' />
+      <Card name='Erwin Smith' />
+      <Card name='Hange Zoë' />
+      <Card name='Levi Ackerman' />
+    </div>
+  )
+}
