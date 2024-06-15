b838ce73cc4bf8bf979afc41cb526e439438cd74
Derek Lim <dereklim3006@gmail.com>
2024-06-15 13:44:55 -0400

    Change to dark theme and enlarge font.

--- memory-card-5/src/Card.jsx
+++ memory-card-5/src/Card.jsx
@@ -37,7 +37,7 @@ export default function Card({
     >
       <img src={url} />
       <div>{name}</div>
-      <div>{`clicked: ${clicked}`}</div>
+      {/* <div>{`clicked: ${clicked}`}</div> */}
     </button>
   )
 }
--- memory-card-5/src/Heading.jsx
+++ memory-card-5/src/Heading.jsx
@@ -2,7 +2,7 @@ export default function Heading({ score, bestScore }) {
   return (
     <div className="heading">
       <div className="sub-heading">
-        <div>AOT Memory Game</div>
+        <div id="title">AOT Memory Game</div>
         <div>Get points by clicking on an image 
           but don't click on any more than once.</div>
       </div>
